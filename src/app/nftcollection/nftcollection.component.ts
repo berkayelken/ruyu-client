8f10301816c8d9945c7848ffb2c1df9f2b0679f3
Gelmez, Ozan, Vodafone (External) <Ozan.Gelmez@vodafone.com>
2024-06-16 22:10:16 +0300

    NFT Collection cards redirect to username based twitter account.

--- src/app/nftcollection/nftcollection.component.ts
+++ src/app/nftcollection/nftcollection.component.ts
@@ -1,77 +1,75 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink  } from '@angular/router';
-import { SlidesOutputData, OwlOptions } from 'ngx-owl-carousel-o';
+import { Component } from '@angular/core'
+import { RouterOutlet, RouterLink } from '@angular/router'
+import { SlidesOutputData, OwlOptions } from 'ngx-owl-carousel-o'
 
-import { CarouselModule } from 'ngx-owl-carousel-o';
-import { CommonModule } from '@angular/common';
+import { CarouselModule } from 'ngx-owl-carousel-o'
+import { CommonModule } from '@angular/common'
 // import { CarouselModule_1 as CarouselModule } from "ngx-owl-carousel-o";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    CommonModule,
-    CarouselModule,
-    RouterOutlet,
-    RouterLink
-  ],
+  imports: [CommonModule, CarouselModule, RouterOutlet, RouterLink],
   templateUrl: './nftcollection.component.html',
-  styleUrl: './nftcollection.component.css'
+  styleUrl: './nftcollection.component.css',
 })
-  
 export class NftcollectionComponent {
   cardString = "../../../assets/nftcollection/card";
-slidesStore: any[] = [];
-currentImages: any[] = [];
+  slidesStore: any[] = []
+  currentImages: any[] = []
+  userNames: String[] = [
+    'voxelqueen',
+    'voxelqueen',
+    'voxelqueen',
+    'voxelqueen',
+    'voxelqueen',
+    'voxelqueen',
+    'voxelqueen',
+    'voxelqueen',
+    'voxelqueen',
+  ]
 
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    for(let i = 1; i < 9; i++) {
-      let image = this.cardString + i + ".webp";
-      this.slidesStore.push({id: i, src: image})
+    for (let i = 1; i <= 9; i++) {
+      let image = this.cardString + i + '.webp'
+      let userName = this.userNames[i - 1]
+      this.slidesStore.push({ id: i, src: image, userName: userName })
     }
 
-    this.loadRandomImages();
+    this.loadRandomImages()
     setInterval(() => {
-      this.loadRandomImages();
-    }, 9000);
+      this.loadRandomImages()
+    }, 9000)
   }
 
   loadRandomImages(): void {
-    let newImages: any[];
+    let newImages: any[]
     do {
-      newImages = this.getRandomImages(3);
-    } while (this.arraysIntersect(newImages, this.currentImages));
+      newImages = this.getRandomImages(3)
+    } while (this.arraysIntersect(newImages, this.currentImages))
 
-    this.currentImages = newImages;
+    this.currentImages = newImages
   }
 
   getRandomImages(count: number): any[] {
-    const randomIndices = this.getRandomIndices(count, this.slidesStore.length);
-    return randomIndices.map(index => this.slidesStore[index]);
+    const randomIndices = this.getRandomIndices(count, this.slidesStore.length)
+    return randomIndices.map((index) => this.slidesStore[index])
   }
 
   getRandomIndices(count: number, max: number): number[] {
-    const indices:any[] = [];
+    const indices: any[] = []
     while (indices.length < count) {
-      const randomIndex = Math.floor(Math.random() * max);
+      const randomIndex = Math.floor(Math.random() * max)
       if (!indices.includes(randomIndex)) {
-        indices.push(randomIndex);
+        indices.push(randomIndex)
       }
     }
-    return indices;
+    return indices
   }
 
   arraysIntersect(arr1: any[], arr2: any[]): boolean {
-    return arr1.some(item1 => arr2.some(item2 => item1.id === item2.id));
+    return arr1.some((item1) => arr2.some((item2) => item1.id === item2.id))
   }
 }
-
-
-
-
- 
-
-
